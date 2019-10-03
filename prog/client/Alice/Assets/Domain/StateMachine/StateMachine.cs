68220de54e6c05c900ff33a45da77107097e885d
kou_yeung <kou_yeung@hotmail.com>
2019-10-03 14:36:51 +0900

    検証的ステートマシンを実装する

---
+++ prog/client/Alice/Assets/Domain/StateMachine/StateMachine.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zoo.State
+{
+
+    //public class StateSystem : MonoBehaviour
+    //{
+    //    static StateSystem instance;
+    //    static List<StateMachine> machines = new List<StateMachine>();
+
+    //    static StateSystem()
+    //    {
+    //        var go = new GameObject("StateSystem");
+    //        instance = go.AddComponent<StateSystem>();
+    //        DontDestroyOnLoad(go);
+    //    }
+
+    //    public static StateMachine Gen()
+    //    {
+    //        var res = new StateMachine();
+    //        machines.Add(res);
+    //        return res;
+    //    }
+    //}
+
+    public class IState<Owner>
+    {
+        public virtual void Begin(Owner owner) { }
+        public virtual void Update(Owner owner, float deltaTime) { }
+        public virtual void End(Owner owner) { }
+    }
+
+    public class StateMachine<Owner>
+    {
+        Dictionary<object, IState<Owner>> stateDic = new Dictionary<object, IState<Owner>>();
+        IState<Owner> currentState;
+        Owner owner;
+
+        public StateMachine(Owner owner)
+        {
+            this.owner = owner;
+        }
+
+        public void Update(float deltaTime)
+        {
+            if (currentState != null) currentState.Update(owner, deltaTime);
+        }
+
+        public void AddState<Key>(Key key, IState<Owner> state)
+        {
+            stateDic.Add(key, state);
+        }
+        public void RemoveState<Key>(Key key)
+        {
+            stateDic.Remove(key);
+        }
+
+        public void ChangeState<Key>(Key key)
+        {
+            if (currentState != null) currentState.End(owner);
+            currentState = stateDic[key];
+            currentState.Begin(owner);
+        }
+
+    }
+}
+
---
+++ prog/client/Alice/Assets/Scenes/Sample.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+using System.Linq;
+using Zoo.State;
+
+public class Sample : MonoBehaviour
+{
+    StateMachine<Sample> machine;
+    enum State
+    {
+        Command,
+        Playback,
+        TurnEnd,
+    }
+    class CommandState : IState<Sample>
+    {
+        float dt = 0;
+        public override void Begin(Sample owner)
+        {
+            dt = 0;
+            Debug.Log("CommandState : Begin");
+        }
+        public override void Update(Sample owner, float deltaTime)
+        {
+            dt += deltaTime;
+            if(dt >= 3)
+            {
+                owner.OnCommand();
+            }
+            Debug.Log("CommandState : Update");
+        }
+    }
+    class PlaybackState : IState<Sample>
+    {
+        public override void Begin(Sample owner)
+        {
+            owner.Para(
+                () => owner.OnPlaybackEnd(),
+                (cb) => owner._Task(5, cb),
+                (cb) => owner._Task(4, cb),
+                (cb) => owner._Task(2, cb),
+                (cb) => owner._Task(3, cb)
+                );
+        }
+    }
+    class TurnEndState : IState<Sample>
+    {
+        public override void Begin(Sample owner)
+        {
+            Debug.Log("TurnEnd : Begin");
+        }
+        public override void Update(Sample owner, float deltaTime)
+        {
+            Debug.Log("TurnEnd : Update");
+        }
+        public override void End(Sample owner)
+        {
+            Debug.Log("TurnEnd : End");
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        machine = new StateMachine<Sample>(this);
+        machine.AddState(State.Command, new CommandState());
+        machine.AddState(State.Playback, new PlaybackState());
+        machine.AddState(State.TurnEnd, new TurnEndState());
+        machine.ChangeState(State.Command);
+
+        // 並列
+        //Para(
+        //    ()=> Debug.Log("End!!"),
+        //    (cb) => _Task(5, cb),
+        //    (cb) => _Task(4, cb),
+        //    (cb) => _Task(2, cb),
+        //    (cb) => _Task(3, cb)
+        //    );
+
+        WF(
+            () => Debug.Log("End!!"),
+            (cb) => _Task(1, cb),
+            (cb) => _Task(4, cb),
+            (cb) => _Task(2, cb),
+            (cb) => _Task(3, cb));
+        //Observable
+        //    .WhenAll
+        //    (
+        //        Observable.FromCoroutine(() => Task(3)),
+        //        Observable.FromCoroutine(() => Task(2)),
+        //        Observable.FromCoroutine(() => Task(4))
+        //    ).Subscribe(_ => Debug.Log("End"));
+
+        //// 直列
+        //Observable
+        //    .FromCoroutine(() => Task(3))
+        //    .SelectMany(() => Task(2))
+        //    .SelectMany(() => Task(4))
+        //    .Subscribe(_ => Debug.Log("End"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        machine.Update(Time.deltaTime);
+    }
+
+    public void OnCommand()
+    {
+        machine.ChangeState(State.Playback);
+    }
+
+    public void OnPlaybackEnd()
+    {
+        machine.ChangeState(State.TurnEnd);
+    }
+
+    IEnumerator Task(float sec)
+    {
+        Debug.Log($"Task Start : {sec}");
+        yield return new WaitForSeconds(sec);
+        Debug.Log($"Task End : {sec}");
+    }
+    IEnumerator TaskWith(float sec, Action cb)
+    {
+        Debug.Log($"Task Start : {sec}");
+        yield return new WaitForSeconds(sec);
+        Debug.Log($"Task End : {sec}");
+        cb();
+    }
+
+    public void _Task(float sec, Action action)
+    {
+        StartCoroutine(TaskWith(sec, action));
+    }
+    void OnCompleted(IObserver<Unit> o)
+    {
+        o.OnCompleted();
+    }
+
+    void Para(Action end, params Action<Action>[] tasks)
+    {
+        // 並列
+        List<IObservable<Unit>> array = new List<IObservable<Unit>>();
+        for (int i = 0; i < tasks.Length; i++)
+        {
+            var task = tasks[i];
+            array.Add(Observable.FromCoroutine<Unit>((o) => Exec(task, () => OnCompleted(o))));
+        }
+        Observable.WhenAll(array).Subscribe(_ => end?.Invoke());
+    }
+
+    void WF(Action end, params Action<Action>[] tasks)
+    {
+        //IObservable<Unit> observable = null;
+
+        //for (int i = 0; i < tasks.Length; i++)
+        //{
+        //    var task = tasks[i];
+        //    if(observable == null)
+        //    {
+        //        observable = Observable.FromCoroutine<Unit>((o) => Exec(task, () => OnCompleted(o)));
+        //    }
+        //    else
+        //    {
+        //        observable = observable.SelectMany(Observable.FromCoroutine<Unit>((o) => Exec(task, () => OnCompleted(o))));
+        //    }
+        //}
+        //observable.Subscribe(_ => end?.Invoke());
+
+        /*
+        // 直列
+        Observable
+            .FromCoroutine(() => tasks)
+            .SelectMany(() => Task(2))
+            .SelectMany(() => Task(4))
+            .Subscribe(_ => Debug.Log("End"));
+            */
+    }
+
+    IEnumerator Exec(Action<Action> action, Action cb)
+    {
+        action(cb);
+        yield break;
+    }
+}
