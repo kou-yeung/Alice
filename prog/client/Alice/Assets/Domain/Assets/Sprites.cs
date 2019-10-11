5f61bb30826b164c208c6996895b837ac5e025b9
kou_yeung <kou_yeung@hotmail.com>
2019-10-11 09:21:14 +0900

    自動分割を借り実装します

--- prog/client/Alice/Assets/Domain/Assets/Sprites.cs
+++ prog/client/Alice/Assets/Domain/Assets/Sprites.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.Reflection;
 
 #if UNITY_EDITOR
 using UnityEditor;
@@ -71,6 +72,45 @@ namespace Zoo.Assets
                 AssetDatabase.CreateAsset(res, path);
             }
             return res;
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void OnPreprocessTexture()
+        {
+            if(assetPath.Contains("AddressableAssets/Character"))
+            {
+                TextureImporter textureImporter = (TextureImporter)assetImporter;
+
+                // Textureのサイズ取得
+                // 参考:http://baba-s.hatenablog.com/entry/2017/06/21/100000
+                var size = new object[2] { 0, 0 };
+                var method = typeof(TextureImporter).GetMethod("GetWidthAndHeight", BindingFlags.NonPublic | BindingFlags.Instance);
+                method.Invoke(textureImporter, size);
+                var width = (int)size[0];
+                var height = (int)size[1];
+
+                textureImporter.spriteImportMode = SpriteImportMode.Multiple;
+                var sliceWidth = width / 3;
+                var sliceHeight = height / 4;
+
+                List<SpriteMetaData> metas = new List<SpriteMetaData>();
+                for (int y = 0; y < 4; y++)
+                {
+                    for (int x = 0; x < 3; x++)
+                    {
+                        var meta = new SpriteMetaData();
+                        meta.pivot = new Vector2(.5f, .5f);
+                        meta.name = $"{Path.GetFileNameWithoutExtension(assetPath)}_{11- (y * 3 + x)}";
+
+                        meta.rect = new Rect(sliceWidth * x, sliceHeight * y, sliceWidth, sliceHeight);
+                        metas.Add(meta);
+                    }
+                }
+                textureImporter.spritesheet = metas.ToArray();
+            }
         }
     }
 #endif
