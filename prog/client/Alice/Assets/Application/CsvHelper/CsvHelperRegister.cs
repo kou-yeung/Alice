0e56303b913cba139dc0bb1cbf201dc8a249b9d7
kou_yeung <kou_yeung@hotmail.com>
2019-10-12 10:27:38 +0900

    マスターデータ追加

---
+++ prog/client/Alice/Assets/Application/CsvHelper/CsvHelperRegister.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using Alice.MasterData;
+
+namespace CsvHelper
+{
+    public class CsvHelperRegister
+    {
+        public static Configuration.Configuration configuration { get; private set; }
+
+        static CsvHelperRegister()
+        {
+            configuration = new Configuration.Configuration();
+            configuration.HeaderValidated = null;
+            AddConverter();
+            RegisterClassMap();
+        }
+
+        /// <summary>
+        /// 型変換の登録
+        /// </summary>
+        static void AddConverter()
+        {
+            //configuration.TypeConverterCache.AddConverter<Identify>(new IdentifyTypeConverter());
+            //configuration.TypeConverterCache.AddConverter<Race>(new EnumTypeConverter<Race>());
+            //configuration.TypeConverterCache.AddConverter<Vector2Int>(new Vector2IntTypeConverter());
+            //configuration.TypeConverterCache.AddConverter<SkillType>(new EnumTypeConverter<SkillType>());
+            //configuration.TypeConverterCache.AddConverter<SkiiTarget>(new EnumTypeConverter<SkiiTarget>());
+        }
+
+        /// <summary>
+        /// クラスマップの登録
+        /// </summary>
+        static void RegisterClassMap()
+        {
+            configuration.RegisterClassMap<CharacterMap>();
+            //configuration.RegisterClassMap<MaterialMap>();
+            //configuration.RegisterClassMap<VendingMap>();
+            //configuration.RegisterClassMap<RecipeMap>();
+            //configuration.RegisterClassMap<ItemMap>();
+            //configuration.RegisterClassMap<ConfigsMap>();
+            //configuration.RegisterClassMap<CheatMap>();
+            //configuration.RegisterClassMap<StringTableMap>();
+            //configuration.RegisterClassMap<DungeonMap>();
+            //configuration.RegisterClassMap<RoomMap>();
+            //configuration.RegisterClassMap<StageMap>();
+            //configuration.RegisterClassMap<LevelMap>();
+            //configuration.RegisterClassMap<EnemyMap>();
+            //configuration.RegisterClassMap<SkillMap>();
+        }
+    }
+}
---
+++ prog/client/Alice/Assets/Application/MasterData/Character.cs
@@ -0,0 +1,19 @@
+﻿using CsvHelper.Configuration;
+
+namespace Alice.MasterData
+{
+    public class Character
+    {
+        public string ID;
+        public string Name;
+    }
+
+    public sealed class CharacterMap : ClassMap<Character>
+    {
+        public CharacterMap()
+        {
+            Map(x => x.ID).Name("ID");
+            Map(x => x.Name).Name("名前");
+        }
+    }
+}
