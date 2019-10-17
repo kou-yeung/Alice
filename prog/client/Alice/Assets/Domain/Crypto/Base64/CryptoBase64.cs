01e33974f71bdb4da941a8f75b60b67cfadc5fdb
kou_yeung <kou_yeung@hotmail.com>
2019-10-17 20:52:20 +0900

    暗号化機能を提供します。（現在はBase64変換するだけです。平文よりましかと。。。

---
+++ prog/client/Alice/Assets/Domain/Crypto/Base64/CryptoBase64.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace Zoo.Crypto
+{
+
+    /// <summary>
+    /// Base64を使って簡易暗号化する
+    /// </summary>
+    public class CryptoBase64 : ICrypto
+    {
+        /// <summary>
+        /// 復号する
+        /// </summary>
+        /// <param name="cipher"></param>
+        /// <returns></returns>
+        public string Decrypt(string cipher)
+        {
+            var bytes = Convert.FromBase64String(cipher);
+            var text = Encoding.UTF8.GetString(bytes);
+            return text;
+        }
+
+        /// <summary>
+        /// 暗号化する
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        public string Encrypt(string text)
+        {
+            var bytes = Encoding.UTF8.GetBytes(text);
+            var cipher = Convert.ToBase64String(bytes);
+            return cipher;
+        }
+    }
+}
---
+++ prog/client/Alice/Assets/Domain/Crypto/Crypto.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zoo.Crypto
+{
+    public interface ICrypto
+    {
+        /// <summary>
+        /// 復号する
+        /// </summary>
+        /// <param name="cipher"></param>
+        /// <returns></returns>
+        string Decrypt(string cipher);
+
+        /// <summary>
+        /// 暗号化する
+        /// </summary>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        string Encrypt(string text);
+    }
+}
