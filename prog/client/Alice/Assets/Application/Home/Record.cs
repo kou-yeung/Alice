9b9288e571a570eab179cc42352665e346d2b143
kou_yeung <kou_yeung@hotmail.com>
2019-10-21 21:02:03 +0900

    試合履歴仮対応します

--- prog/client/Alice/Assets/Application/Home/Record.cs
+++ prog/client/Alice/Assets/Application/Home/Record.cs
@@ -1,21 +1,76 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Xyz.AnzFactory.UI;
+using Zoo;
 
 namespace Alice
 {
-    public class Record : MonoBehaviour
+    public class Record : MonoBehaviour, ANZListView.IDataSource, ANZListView.IActionDelegate
     {
         public Battle battle;
+        public ANZListView recordTable;
+        public GameObject recordItemPrefab;
 
-        // 仮
-        System.Random random = new System.Random();
-        public void OnClick()
+        void Start()
+        {
+            PrefabPool.Regist(RecordItem.PrefabKey, recordItemPrefab);
+            recordTable.DataSource = this;
+            recordTable.ActionDelegate = this;
+        }
+
+        /// <summary>
+        /// リスト更新する
+        /// </summary>
+        public void ReloadData()
+        {
+            recordTable.ReloadData();
+        }
+
+        /// <summary>
+        /// 履歴数
+        /// </summary>
+        /// <returns></returns>
+        public int NumOfItems()
+        {
+            return UserData.GetBattleRecord().Count;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        public float ItemSize()
+        {
+            var rectTransform = recordItemPrefab.GetComponent<RectTransform>();
+            return rectTransform.sizeDelta.y;
+        }
+
+        /// <summary>
+        /// 試合履歴アイテムx1の表示物が要求された
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public GameObject ListViewItem(int index, GameObject item)
+        {
+            if (item == null)
+            {
+                item = RecordItem.Gen().gameObject;
+            }
+            var record = UserData.GetBattleRecord();
+            item.GetComponent<RecordItem>().Setup(record[index]);
+            return item;
+        }
+
+        /// <summary>
+        /// タップした
+        /// </summary>
+        /// <param name="index"></param>
+        /// <param name="listItem"></param>
+        public void TapListItem(int index, GameObject listItem)
         {
             var record = UserData.GetBattleRecord();
-            if (record.Count <= 0) return;
-            var index = random.Next(0, record.Count);
-            // 履歴再生する
             battle.Exec(record[index], true);
         }
     }
---
+++ prog/client/Alice/Assets/Application/Home/RecordItem.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zoo;
+
+namespace Alice
+{
+    /// <summary>
+    /// 試合履歴アイテムx1
+    /// </summary>
+    public class RecordItem : MonoBehaviour
+    {
+        public static readonly string PrefabKey = "RecordItem";
+        public Text title;
+
+        BattleStartRecv recv;
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Setup(BattleStartRecv recv)
+        {
+            this.recv = recv;
+            title.text = this.recv.seed.ToString();
+        }
+
+        /// <summary>
+        /// RecordItem生成
+        /// </summary>
+        /// <returns></returns>
+        public static RecordItem Gen()
+        {
+            var go = PrefabPool.Get(PrefabKey);
+            var res = go.GetComponent<RecordItem>();
+            return res;
+        }
+    }
+}
