0d44d9517d17f19fa8bf3b19f529c9bc579ec95a
kou_yeung <kou_yeung@hotmail.com>
2019-10-22 11:04:35 +0900

    ユニット4を表示するためのカードを作成する

--- prog/client/Alice/Assets/Application/Home/RecordItem.cs
+++ prog/client/Alice/Assets/Application/Home/RecordItem.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Zoo;
+using System.Text;
 
 namespace Alice
 {
@@ -18,10 +19,33 @@ namespace Alice
         /// <summary>
         /// 
         /// </summary>
+        static StringBuilder sb = new StringBuilder();
         public void Setup(BattleStartRecv recv)
         {
             this.recv = recv;
-            title.text = this.recv.seed.ToString();
+
+            sb.Clear();
+            if(this.recv.names != null)
+            {
+                sb.AppendLine($"{this.recv.names[0]} vs. {this.recv.names[1]}");
+            }
+            switch (this.recv.result)
+            {
+                case BattleConst.Result.Unknown:
+                case BattleConst.Result.Draw:
+                    sb.AppendLine($"{this.recv.result}");
+                    break;
+                case BattleConst.Result.Win:
+                    sb.AppendLine($"<color=red>WIN</color>");
+                    break;
+                case BattleConst.Result.Lose:
+                    sb.AppendLine($"<color=blue>LOSE</color>");
+                    break;
+            }
+            sb.AppendLine($"----------------------------");
+            sb.AppendLine($"ID:{this.recv.seed}");
+
+            title.text = sb.ToString();
         }
 
         /// <summary>
