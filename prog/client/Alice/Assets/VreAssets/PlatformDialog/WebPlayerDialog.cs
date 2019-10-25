7b20a00c1f59161e0d50384e10807a61fcce25ba
kou_yeung <kou_yeung@hotmail.com>
2019-10-25 13:53:29 +0900

    ダイアログ表示するPluginsを追加します

---
+++ prog/client/Alice/Assets/VreAssets/PlatformDialog/WebPlayerDialog.cs
@@ -0,0 +1,56 @@
+﻿#if UNITY_WEBPLAYER
+using UnityEngine;
+using System.Collections;
+
+public class WebPlayerDialog : IDialogInterface {
+	private string javascriptInitialize = @"
+	";
+
+	private string javascriptConfirm = @"
+		var unity = u.getUnity();
+		if(window.confirm('MESSAGE')){
+			unity.SendMessage('PlatformDialog', 'OnPositive', 'positive');
+		}
+		else{
+			unity.SendMessage('PlatformDialog', 'OnNegative', 'positive');
+		}";
+
+	private string javascriptAlert = @"
+		var unity = u.getUnity();
+		if(window.alert('MESSAGE')){
+			unity.SendMessage('PlatformDialog', 'OnPositive', 'positive');
+		}
+		else{
+			unity.SendMessage('PlatformDialog', 'OnNegative', 'positive');
+		}";
+
+	public WebPlayerDialog() {
+		// Application.ExternalEval( javascriptInitialize );
+	}
+
+	public void Show (string message, PlatformDialog.Type buttonType) {
+		if( buttonType == PlatformDialog.Type.SubmitOnly ) {
+			Application.ExternalEval( javascriptAlert.Replace("MESSAGE", message) );
+		}
+		else {
+			Application.ExternalEval( javascriptConfirm.Replace("MESSAGE", message) );
+		}
+	}
+
+	public void Show (string title, string message, PlatformDialog.Type buttonType) {
+		Show(message, buttonType);
+	}
+
+	public void SetButtonLabel( string positive ) {
+
+	}
+
+	public void SetButtonLabel( string positive, string negative ) {
+
+	}
+
+	public void Dismiss() {
+
+	}
+}
+#endif
---
+++ prog/client/Alice/Assets/VreAssets/PlatformDialog/iOSDialog.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class iOSDialog : IDialogInterface {
+#if UNITY_IPHONE && !UNITY_EDITOR
+	[DllImport("__Internal")]
+    private static extern void DialogShowWithMessage (string message, int buttonType);
+	[DllImport("__Internal")]
+	private static extern void DialogShowWithTitle (string title, string message, int buttonType);
+	[DllImport("__Internal")]
+	private static extern void DialogSetButtonLabel( string positive );
+	[DllImport("__Internal")]
+	private static extern void DialogSetButtonLabels( string positive, string negative );
+	[DllImport("__Internal")]
+	private static extern void DialogDismiss();
+	
+	
+
+	public void Show (string message, PlatformDialog.Type buttonType) {
+		DialogShowWithMessage(message, buttonType.ToInt() );
+	}
+
+	public void Show (string title, string message, PlatformDialog.Type buttonType) {
+		DialogShowWithTitle( title, message, buttonType.ToInt() );
+	}
+
+	public void SetButtonLabel( string positive ) {
+		DialogSetButtonLabel(positive);
+	}
+	public void SetButtonLabel( string positive, string negative ) {
+		DialogSetButtonLabels(positive,negative);
+	}
+
+	public void Dismiss() {
+		DialogDismiss();
+	}
+#else
+	public void Show (string message, PlatformDialog.Type buttonType) {}
+	public void Show (string title, string message, PlatformDialog.Type buttonType) {}
+	public void SetButtonLabel( string positive ) {}
+	public void SetButtonLabel( string positive, string negative ) {}
+	public void Dismiss() {}
+#endif
+}
