5cddd66b57fbe938e3d8fb5ff435277ba1cd842e
kou_yeung <kou_yeung@hotmail.com>
2019-11-02 22:04:21 +0900

    シャドウ画面を「検索」「生成」トグルで分けます

--- prog/client/Alice/Assets/Application/Home/VS.cs
+++ prog/client/Alice/Assets/Application/Home/VS.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Zoo.Communication;
 
 namespace Alice
@@ -8,6 +9,8 @@ namespace Alice
     public class VS : MonoBehaviour
     {
         public Battle battle;
+        public InputField input;
+        public Button btnFind;
 
         /// <summary>
         /// シャドウを生成する
@@ -22,12 +25,40 @@ namespace Alice
 
         public void OnBattle()
         {
-            var c2s = new ShadowBattleSend(630);
+            int roomid = 0;
+            int.TryParse(input.text.TrimStart('0'), out roomid);
+            var c2s = new ShadowBattleSend(roomid);
             CommunicationService.Instance.Request("BattleShadow", JsonUtility.ToJson(c2s), res =>
             {
                 UserData.editedUnit.Clear();    // 同期しました
-                battle.Exec(JsonUtility.FromJson<BattleStartRecv>(res));
+                var s2c = JsonUtility.FromJson<BattleStartRecv>(res);
+                if(s2c.type == BattleConst.BattleType.Shadow)
+                {
+                    battle.Exec(s2c);
+                } else
+                {
+                    // ヒットしませんでした。
+                    PlatformDialog.SetButtonLabel("OK");
+                    PlatformDialog.Show(
+                        "確認",
+                        "指定されたIDが存在しません",
+                        PlatformDialog.Type.SubmitOnly,
+                        () => {
+                            Debug.Log("OK");
+                        }
+                    );
+                }
             });
+        }
+
+        /// <summary>
+        /// ルームIDを変更しました
+        /// </summary>
+        /// <param name="input"></param>
+        public void OnEndEdit(InputField input)
+        {
+            // ５文字以上入力したら有効
+            btnFind.interactable = input.text.Length >= 5;
         }
     }
 }
