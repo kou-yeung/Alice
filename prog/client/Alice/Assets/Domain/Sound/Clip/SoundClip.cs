655542dab121be3bb14925ba23744c59ad2c2cff
kou_yeung <kou_yeung@hotmail.com>
2019-12-07 14:08:58 +0900

    サウンド再生のサービスロケータを提供します

---
+++ prog/client/Alice/Assets/Domain/Sound/Clip/SoundClip.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Audio;
+using Zoo.IO;
+using System.Linq;
+
+namespace Zoo.Sound
+{
+    public class SoundClip : ISound
+    {
+        GameObject audioSources;
+
+        AudioSource bgmSource;
+        List<AudioSource> seSources = new List<AudioSource>();
+
+        public SoundClip(int maxSe = 64)
+        {
+            audioSources = new GameObject("AudioSources");
+            GameObject.DontDestroyOnLoad(audioSources);
+
+            // AudioSourceを作成しておく : BGM
+            bgmSource = audioSources.AddComponent<AudioSource>();
+            bgmSource.loop = true;
+
+            // AudioSourceを作成しておく : SE
+            for (int i = 0; i < maxSe; i++)
+            {
+                var source = audioSources.AddComponent<AudioSource>();
+                seSources.Add(source);
+            }
+        }
+
+        public void PlayBGM(string path)
+        {
+            LoaderService.Instance.LoadAsync<AudioClip>(path, (clip) =>
+            {
+                bgmSource.clip = clip;
+                bgmSource.Play();
+            });
+        }
+
+        public void PlaySE(string path)
+        {
+            LoaderService.Instance.LoadAsync<AudioClip>(path, (clip) =>
+            {
+                var source = seSources.FirstOrDefault(v => !v.isPlaying);
+                if(source != null)
+                {
+                    source.clip = clip;
+                    source.Play();
+                }
+            });
+        }
+    }
+}
---
+++ prog/client/Alice/Assets/Domain/Sound/Sound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Zoo.Sound
+{
+    public interface ISound
+    {
+        void PlayBGM(string path);
+        void PlaySE(string path);
+    }
+
+    /// <summary>
+    /// Soundサービスを提供する
+    /// </summary>
+    public class SoundService : ServiceLocator<ISound> { }
+}
+
