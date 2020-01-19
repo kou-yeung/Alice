4ebfbdf756c3d378fdb35c3477ee36194a72740c
kou_yeung <kou_yeung@hotmail.com>
2020-01-19 20:51:53 +0900

    レベル計算ロジックを追加

---
+++ prog/client/Alice/Assets/Application/Logic/Level.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Alice.Logic
+{
+    public static class LevelTable
+    {
+        public static int Exp2Level(int exp)
+        {
+            return Mathf.FloorToInt(Mathf.Sqrt(exp)) + 1;
+        }
+
+        public static int LevelExp(int level)
+        {
+            return level * level;
+        }
+    }
+}
